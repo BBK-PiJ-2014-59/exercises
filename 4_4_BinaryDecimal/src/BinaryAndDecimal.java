0aff24f9bb87149d036d3a459ab7703d8bf6c896
j.oldershaw <j.oldershaw@ucl.ac.uk>
2013-10-21 20:38:32 +0100

    skeleton

---
+++ 4_4_BinaryDecimal/src/BinaryAndDecimal.java
@@ -0,0 +1,33 @@
+public class BinaryAndDecimal {
+    
+    public void launch() {
+        printMenu();
+        getSelectionFromUser();
+        runSelectedConversion();        
+    }
+    
+    void printMenu() {
+        
+    }
+    
+    void getSelectionFromUser() {
+        
+    }
+    
+    void runSelectedConversion() {
+        
+    }
+    
+    int binary2decimal(String b) {
+        return 0;
+    }
+    
+    int decimal2binary (String d) {
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        BinaryAndDecimal session = new BinaryAndDecimal();
+        session.launch();
+    }
+}
