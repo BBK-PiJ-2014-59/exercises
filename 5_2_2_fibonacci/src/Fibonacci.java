6654882ee52c65b19910c846e9afdc66408b8206
j.oldershaw <j.oldershaw@ucl.ac.uk>
2013-10-26 17:11:53 +0100

    works.

--- 5_2_2_fibonacci/src/Fibonacci.java
+++ 5_2_2_fibonacci/src/Fibonacci.java
@@ -1,6 +1,32 @@
 import java.util.*;
 
 public class Fibonacci {
+    static Map<Integer, Integer> calculated = new HashMap<Integer, Integer>();
+
+  int fib3(int n) {
+          Fibonacci.calculated.put(1, 1); 
+          Fibonacci.calculated.put(2, 1); 
+          int fibn2, fibn1;
+          if (n == 1 || n == 2) {
+            return 1;
+          } else {
+            // int curfib = Fibonacci.fibs.get(n - 2) + Fibonacci.fibs.get(n - 1);
+            if (Fibonacci.calculated.containsKey(n - 2)) {
+              fibn2 = Fibonacci.calculated.get(n - 2);
+            } else {
+              fibn2 = fib3(n - 2);
+              Fibonacci.calculated.put(n - 2, fibn2);
+            }
+            if (Fibonacci.calculated.containsKey(n - 1)) {
+              fibn1 = Fibonacci.calculated.get(n - 1);
+            } else {
+              fibn1 = fib3(n - 1);
+              Fibonacci.calculated.put(n - 1, fibn1);
+            }
+            return fibn2 + fibn1; 
+          } 
+  }
+
   int fib(int n) {
     if (n == 1 || n == 2) {
       return 1;
@@ -8,6 +34,7 @@ public class Fibonacci {
       return fib(n - 2) + fib(n - 1); 
     }
   }
+
   int fib2(int n) {
     // 1, 1, 1 + 1, 1 + (1 + 1), 1 + (1 + (1 + 1)), 1 + (1 + (1 + (1 + 1))) 
     List<Integer> fibs = new ArrayList<Integer>();
@@ -25,6 +52,6 @@ public class Fibonacci {
 
   public static void main(String[] args) {
     Fibonacci f = new Fibonacci();
-    System.out.println(f.fib2(45));
+    System.out.println(f.fib3(45));
   }
 }
