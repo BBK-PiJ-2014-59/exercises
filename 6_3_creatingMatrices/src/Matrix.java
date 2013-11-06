4d78c5132c77b74add80ada65ae53e00ee0ce994
jesse <jesse@ubuntu.ubuntu-domain>
2013-11-06 20:53:45 +0000

    works

--- 6_3_creatingMatrices/src/Matrix.java
+++ 6_3_creatingMatrices/src/Matrix.java
@@ -99,7 +99,7 @@ public class Matrix {
     System.out.println(m[3][1]);
     System.out.println(toString2());
     prettyPrint();
-    setMatrix("1,2,3,4;5,6,7,8;9,10,11,12;13,14,15,16;17,18,19,20");
+    setMatrix("1,2,3,4,5;6,7,8,9,10;11,12,13,14,15;16,17,18,19,20");
     System.out.println();
     prettyPrint();
   }
