240767911ca022e3efc276fcb4646fc8a24038ba
j <folde01@students.bbk.ac.uk>
2014-02-26 21:32:50 +0000

    made battery of tests. They all pass.

--- 12_testing/d8_StringStack/StringStackTest.java
+++ 12_testing/d8_StringStack/StringStackTest.java
@@ -3,59 +3,60 @@ import static org.junit.Assert.*;
 
 public class StringStackTest {
   
+  String str1 = "foo1";
+  String str2 = "foo2";
+  String str3 = "foo3";
+
+  StringStack stack;
+
+  @Before
+  public void initialize() {
+    stack = new PointerStringStack();
+  }  
 
   @Test
-  public void testsPushEmptyStack() {
+  public void testsPushTwoOntoEmptyStackThenPopThem() {
+    stack.push(str1);
+    stack.push(str2);
+    assertSame(str2, stack.pop()); 
+    assertSame(str1, stack.pop()); 
+    assertNull(stack.pop()); 
   }
-
-  @Test
-  public void testsPushStackOfOne() {
-  }
-
-  @Test
-  public void testsPushStackOfMany() {
-  }
-
-  //
-
-  @Test
-  public void testsPopEmptyStack() {
-  }
-
-  @Test
-  public void testsPopStackOfOne() {
-  }
-
-  @Test
-  public void testsPopStackOfMany() {
-  }
-
-  //
 
   @Test
   public void testsPeekEmptyStack() {
+    assertNull(stack.peek()); 
   }
+
 
   @Test
   public void testsPeekStackOfOne() {
+    stack.push(str1);
+    assertSame(str1, stack.peek()); 
+    assertSame(str1, stack.pop()); 
   }
 
   @Test
   public void testsPeekStackOfMany() {
+    stack.push(str1);
+    stack.push(str2);
+    assertSame(str2, stack.peek()); 
+    assertSame(str2, stack.pop()); 
+    assertSame(str1, stack.peek()); 
+    assertSame(str1, stack.pop()); 
   }
 
-  //
 
   @Test
   public void testsIsEmptyEmptyStack() {
+    assertTrue(stack.isEmpty()); 
   }
+
 
   @Test
   public void testsIsEmptyStackOfOne() {
-  }
-
-  @Test
-  public void testsIsEmptyStackOfMany() {
+    stack.push(str1);
+    assertFalse(stack.isEmpty()); 
   }
 
 } 
