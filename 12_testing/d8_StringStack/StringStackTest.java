6ce17c2d84eb760f1a23aef016827d5722c797e0
j <folde01@students.bbk.ac.uk>
2014-02-26 19:43:02 +0000

    added method names for a bunch of tests

---
+++ 12_testing/d8_StringStack/StringStackTest.java
@@ -0,0 +1,61 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class StringStackTest {
+  
+
+  @Test
+  public void testsPushEmptyStack() {
+  }
+
+  @Test
+  public void testsPushStackOfOne() {
+  }
+
+  @Test
+  public void testsPushStackOfMany() {
+  }
+
+  //
+
+  @Test
+  public void testsPopEmptyStack() {
+  }
+
+  @Test
+  public void testsPopStackOfOne() {
+  }
+
+  @Test
+  public void testsPopStackOfMany() {
+  }
+
+  //
+
+  @Test
+  public void testsPeekEmptyStack() {
+  }
+
+  @Test
+  public void testsPeekStackOfOne() {
+  }
+
+  @Test
+  public void testsPeekStackOfMany() {
+  }
+
+  //
+
+  @Test
+  public void testsIsEmptyEmptyStack() {
+  }
+
+  @Test
+  public void testsIsEmptyStackOfOne() {
+  }
+
+  @Test
+  public void testsIsEmptyStackOfMany() {
+  }
+
+} 
