a7e86adb0c687d06130b69c63e5f5058936b81ce
j <folde01@students.bbk.ac.uk>
2014-02-26 18:57:39 +0000

    bug fixed. test passed.

--- 12_testing/Initials.java
+++ 12_testing/Initials.java
@@ -1,7 +1,7 @@
 public class Initials {
   public static String getInitials(String fullName) {
     String result = "";
-    String[] words = fullName.split(" ");
+    String[] words = fullName.split("\\s+");
     for (int i = 0; i < words.length; i++) {
        String nextInitial = "" + words[i].charAt(0);
        result = result + nextInitial.toUpperCase();
