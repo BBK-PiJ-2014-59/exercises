99c50556f037d442d72d245e02c14371b5885497
j <folde01@students.bbk.ac.uk>
2014-03-29 22:44:50 +0000

    Some tests of PointerPersonQueue.java pass now.

--- 12_testing/d8_StacksAndQueues/PersonQueueTest.java
+++ 12_testing/d8_StacksAndQueues/PersonQueueTest.java
@@ -6,21 +6,49 @@ public class PersonQueueTest {
   PersonQueue pq;
   Person p1;
   Person p2;
+  Person p3;
 
   @Before
   public void buildUp() {
-    pq = new PersonQueuePointer();
+    pq = new PointerPersonQueue();
     p1 = new Person("Yoda");
     p2 = new Person("Hamish");
+    p3 = new Person("Billy");
   }
 
   @Test
-  public void testsInsertAndRetrieve() {
+  public void testsEmptyQueue() { 
     assertNull(pq.retrieve());
-
     pq.insert(p1);
     assertEquals(p1, pq.retrieve());
     assertNull(pq.retrieve());
+  }
+
+  @Test
+  public void testsQueueOfOne() {
+    pq.insert(p1);
+    pq.insert(p2);
+    System.out.println("RETRIEVE 1");
+    Person r1 = pq.retrieve();
+    assertEquals(p1, r1);
+    System.out.println("RETRIEVE 2");
+    Person r2 = pq.retrieve();
+    assertEquals(p2, r2);
+    assertNull(pq.retrieve());
+  }
+
+  @Test
+  public void testsQueueOfTwo() {
+    pq.insert(p1);
+    pq.insert(p2);
+    pq.insert(p3);
+    assertEquals(p1, pq.retrieve());
+    assertEquals(p2, pq.retrieve());
+    assertEquals(p3, pq.retrieve());
+    assertNull(pq.retrieve());
+  }
+
+
 
     /*
     pq.insert(p1);
@@ -29,6 +57,5 @@ public class PersonQueueTest {
     assertEquals(p2, pq.retrieve());
     assertNull(pq.retrieve());
     */
-  }
 
 }
