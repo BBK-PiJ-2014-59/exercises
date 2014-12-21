d1bc191aa55f08a3b6155ecc565a66301b7dba17
jesse <folde01@dcs.bbk.ac.uk>
2014-12-21 21:04:38 +0000

    TestHashUtilities: seems ok

--- 0day12-testing/HashUtilities.java
+++ 0day12-testing/HashUtilities.java
@@ -1,14 +1,18 @@
 public class HashUtilities { 
   static int shortHash(int n) { 
-    return Math.abs(n % 1000);
+    return Math.abs(n % 1000) + 1;
   }
 
   public static void main(String[] args) { 
+    /*
     System.out.println("Give me a string and I will calculate its hash code");
     String str = System.console().readLine();
+    //str="";
     int hash = str.hashCode();
     int smallHash = HashUtilities.shortHash(hash);
     System.out.println("hashCode: " + hash + " ---- 0 < " + smallHash + " < 1000");
+    */
+    System.out.println(HashUtilities.shortHash(0));
 
     // Tests:
     
