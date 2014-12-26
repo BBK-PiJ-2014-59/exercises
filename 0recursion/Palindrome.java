81bc41c51d983a797cf56caff7b81461fa242877
jesse <folde01@dcs.bbk.ac.uk>
2014-12-26 18:03:08 +0000

    Palindrome: seems to work

--- 0recursion/Palindrome.java
+++ 0recursion/Palindrome.java
@@ -1,16 +1,20 @@
 class Palindrome { 
   static boolean p(String s) { 
-    System.out.println(s.substring(0,1));
-    System.out.println(s.substring(s.length()-1,s.length()));
-    if (s.substring(0,1).equals(s.substring(s.length()-1,s.length())))  
-      return true;
-    else return false;
+    //System.out.println(s.substring(0,1));
+    //System.out.println(s.substring(s.length()-1,s.length()));
+
+    if (s.length() <= 1) return true;
+    if (!s.substring(0,1).equals(s.substring(s.length()-1,s.length())))  
+      return false;
+    else return p(s.substring(1,s.length()-1));
   }
   public static void main(String[] args) { 
-    System.out.println(p("a"));
+    System.out.println(p("abcccbba"));
+    /*
     System.out.println(p("aa"));
     System.out.println(p("ab"));
     System.out.println(p("aba"));
     System.out.println(p("abc"));
+    */
   }
 }
