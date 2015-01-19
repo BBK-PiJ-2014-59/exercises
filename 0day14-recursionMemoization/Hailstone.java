14ee65561073cecd8c9ba4d2cd519072e28052b3
J O <folde01@bbk.ac.uk>
2015-01-19 10:28:09 +0000

    Hailstone: seems to work

--- 0day14-recursionMemoization/Hailstone.java
+++ 0day14-recursionMemoization/Hailstone.java
@@ -7,27 +7,34 @@ import java.util.ArrayList;
   * It is thought that this sequence always converges to 1 (this is the Collatz conjecture, not demonstrated yet).  Write a method that returns a list of integers (hint: you can use List<Integer> and ArrayList<Integer>) with the sequence of hailstone numbers that starts at some given natural number provided by the user.
   */
 public class Hailstone { 
+
+  private static List<Integer> result = new ArrayList<Integer>();
+
   public static List<Integer> hail(int n) { 
-    List result = new ArrayList();
+    //List<Integer> result = new ArrayList<Integer>();
     if (n == 1) { 
-      System.out.println("yoda 1");
+      //System.out.println("yoda 1");
       result.add(1);
+      //System.out.println("result 1: " + result);
     }
     else if (n % 2 == 0) { 
       int next = n/2;
-      System.out.println("yoda even: " + next);
-      for (int i : hail(next))
-        result.add(i);
+      //System.out.println("yoda even: " + next);
+      result.add(next);
+      hail(next);
+      //System.out.println("result E: " + result);
     } else { 
       int next = 3*n + 1;
-      System.out.println("yoda odd: " + next);
-      for (int i : hail(next))
-        result.add(i);
+      //System.out.println("yoda odd: " + next);
+      result.add(next);
+      hail(next);
+      //System.out.println("result O: " + result);
     }
     return result;
   }
   public static List<Integer> hail2(int n) { 
-    List result = new ArrayList();
+    //System.out.println("--HAIL2--");
+    List<Integer> result = new ArrayList<Integer>();
     int next;
     if (n == 1) { 
       next = 1;
@@ -47,6 +54,7 @@ public class Hailstone {
   public static void main(String[] args) { 
     int start = Integer.parseInt(System.console().readLine("Enter a number: "));
     System.out.println("hail " + start + ": " + Hailstone.hail(start));
-    System.out.println("hail2 " + start + ": " + Hailstone.hail2(start));
+    //System.out.println("");
+    //System.out.println("hail2 " + start + ": " + Hailstone.hail2(start));
   }
 }
