42ebb90b07b11a75ede65e6d30ce131d7fc51bcb
J O <folde01@bbk.ac.uk>
2015-02-14 14:51:31 +0000

    TextLoopExecImpl.java: add a task queue

---
+++ 0day18-concurrency2/ExecutorImpl.java
@@ -0,0 +1,22 @@
+/*
+3 Responsive UI (that degrades gracefully)
+3.1 Implement Executor
+Create your own implementation of Executor. Look at the documentation of the API of Executor for ideas. Your
+implementation must be able to execute one Runnable at a time (but you can make it run several tasks in paralel,
+see also Exercise 6) and must have a queue of pending tasks.
+Try your implementation by exchaning the class that you used in your solution for the former exercise by your
+new implementation of Executor.
+*/
+
+import java.util.concurrent.Executor;
+
+class ExecutorImpl implements Executor {
+  /*
+     public void execute(Runnable r) {
+         r.run();
+     }
+  */
+  public void execute(Runnable r) {
+    new Thread(r).start();
+  }
+}
